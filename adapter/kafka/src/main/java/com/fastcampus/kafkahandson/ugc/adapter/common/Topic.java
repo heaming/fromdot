daff0c6e375a9f60855a313dc4898596e85291b7
heaming <meetoohi@gmail.com>
2024-09-10 01:28:36 +0900

    [2024-09-09] chatgpt-kafka / consumer 설정

--- adapter/kafka/src/main/java/com/fastcampus/kafkahandson/ugc/adapter/common/Topic.java
+++ adapter/kafka/src/main/java/com/fastcampus/kafkahandson/ugc/adapter/common/Topic.java
@@ -1,7 +1,8 @@
 package com.fastcampus.kafkahandson.ugc.adapter.common;
 
 public class Topic {
-    public static final String ORIGINAL_TOPIC = "post.original";
+    public static final String ORIGINAL_POST = "post.original";
+    public static final String INSPECTED_POST = "post.inspected";
 
 
 }
