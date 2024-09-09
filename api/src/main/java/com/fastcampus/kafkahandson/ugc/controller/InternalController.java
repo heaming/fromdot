1c58a5a7aeafe6fab9c1483521a66bcea9b40c78
heaming <meetoohi@gmail.com>
2024-09-09 16:42:48 +0900

    [2024-09-09] chatgpt open api 연결

---
+++ api/src/main/java/com/fastcampus/kafkahandson/ugc/controller/InternalController.java
@@ -0,0 +1,22 @@
+package com.fastcampus.kafkahandson.ugc.controller;
+
+import com.fastcampus.kafkahandson.ugc.port.TestChatGptPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/internal")
+public class InternalController {
+
+    private final TestChatGptPort testChatGptPort;
+
+    @GetMapping
+    String test(@RequestParam("content") String content) {
+        return testChatGptPort.test(content);
+    }
+
+}
---
+++ usecase/core/src/main/java/com/fastcampus/kafkahandson/ugc/port/TestChatGptPort.java
@@ -0,0 +1,6 @@
+package com.fastcampus.kafkahandson.ugc.port;
+
+public interface TestChatGptPort {
+
+    String test(String content);
+}
